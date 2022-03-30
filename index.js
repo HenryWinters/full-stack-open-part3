c0b3ffab25680987c32b75c2a011bd34e368e40a
Henry Winters <hlwinters17@gmail.com>
2022-03-29 19:34:58 -0700

    Added files for heroku

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express() 
+app.use(cors())
 app.use(express.json())
 morgan.token('post', function (request, response) { return JSON.stringify(request.body) })
 app.use(morgan(function (tokens, req, res) {
@@ -116,7 +118,7 @@ app.post('/api/persons', (request, response) => {
 
 app.use(morgan('tiny'))
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
